79d08e784fd188f7a8fe88ff7a74bfd191102aa6
Jason Scholl <jason.e.scholl@gmail.com>
2020-09-08 19:26:26 -0700

    Upgrade to php7.4

--- bin/my_init.d/set_php_tz.sh
+++ bin/my_init.d/set_php_tz.sh
@@ -7,18 +7,18 @@ if [[ $CUSTOM_PHP_INI != 'true' ]] ; then
     if [ -f /etc/container_environment/TZ ] ; then
       sed -i \
           "s#\;date\.timezone\ \=#date\.timezone\ \=\ $TZ#g" \
-          /etc/php/7.2/cli/php.ini
+          /etc/php/7.4/cli/php.ini
       sed -i \
           "s#\;date\.timezone\ \=#date\.timezone\ \=\ $TZ#g" \
-          /etc/php/7.2/apache2/php.ini
+          /etc/php/7.4/apache2/php.ini
     else
       echo "Timezone not specified by environment variable"
       echo UTC > /etc/container_environment/TZ
       sed -i \
           "s#\;date\.timezone\ \=#date\.timezone\ \=\ UTC#g" \
-          /etc/php/7.2/cli/php.ini
+          /etc/php/7.4/cli/php.ini
       sed -i \
           "s#\;date\.timezone\ \=#date\.timezone\ \=\ UTC#g" \
-          /etc/php/7.2/apache2/php.ini
+          /etc/php/7.4/apache2/php.ini
     fi
 fi
